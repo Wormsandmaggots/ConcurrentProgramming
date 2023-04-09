f7fd9d4780aaebd9c84756f839165b7d8e6d2e30
Wormsandmaggots <Sobrado@o2.pl>
2023-04-09 12:37:13 +0200

    Ball logic test

--- LogicTests/BallLogicTest.cs
+++ LogicTests/BallLogicTest.cs
@@ -7,11 +7,17 @@ namespace LogicTests
     public class BallLogicTest
     {
         private int _x, _y, _radius;
+        private int _xBorder, _yBorder;
+        private int _moveDistance;
 
         [TestInitialize]
         public void Initialize()
         {
             _radius = 1;
+            _moveDistance = 1;
+
+            _x = 5;
+            _y = 5;
         }
 
         [TestMethod]
@@ -25,6 +31,7 @@ namespace LogicTests
             Assert.AreEqual(b.Radius, _radius);
         }
 
+        [TestMethod]
         public void ChangeBallLogicParametres()
         {
             BallLogic b = new BallLogic(_x, _y, _radius);
@@ -32,8 +39,22 @@ namespace LogicTests
             b.X = 1;
             b.Y = 1;
 
-            Assert.AreEqual(b.X, _x);
-            Assert.AreEqual(b.Y, _y);
+            Assert.AreEqual(b.X, 1);
+            Assert.AreEqual(b.Y, 1);
+        }
+
+        [TestMethod]
+        public void MoveBallTest()
+        {
+            BallLogic b = new BallLogic(_x, _y, _radius);
+
+            for(int i = 0; i < 10; i++)
+            {
+                b.MoveBallRandomly(_xBorder, _yBorder, _moveDistance);
+
+                Assert.IsTrue(b.X > _radius && b.X < _xBorder - _radius);
+                Assert.IsTrue(b.Y > _radius && b.Y < _yBorder - _radius);
+            }
         }
     }
 }
