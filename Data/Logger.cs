8667daec8bb220020c2e24023f85cb4abc93a198
AgnieszkaStasiak123 <stasiak.agnieszka567@gmail.com>
2023-06-13 01:28:33 +0200

    First Logger draft

---
+++ Data/Logger.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Data
+{
+    internal class Logger
+    {
+        private readonly string _pathToFile;
+        //private static List<IBall> balls;
+        private Stopwatch stopWatch = new Stopwatch();
+        XmlSerializer serializer = new XmlSerializer(typeof(IBall));
+        private static ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+        private readonly Mutex queueMutex = new();
+
+        public Logger(/*List<IBall> balls*/)
+        {
+            string PathToSave = Path.GetTempPath();
+            _pathToFile = PathToSave + "Logging.xml";
+
+            FileStream file = File.Create(_pathToFile);
+            StreamWriter sw = new StreamWriter(file);
+            /*string data = "{\"BallsDiameter\": 20, ": ";
+            sw.WriteLine(data);
+            sw.Flush();
+            sw.Close();*/
+
+
+
+            // balls = balls;
+        }
+
+
+        public void ToQueue(IBall ball)
+        {
+            stopWatch.Stop();
+            TimeSpan timeLasted = stopWatch.Elapsed;
+            stopWatch.Start();
+            string time = timeLasted.ToString();
+            string id = ball.GetHashCode().ToString();
+
+            var ballJson = new
+            {
+                Xvalue = ball.X.ToString(),
+                Yvalue = ball.Y.ToString(),
+                TimeElapsed = time.ToString(),
+                BallID = id
+            };
+
+            /*  XmlDictionary xmlDictionary = new XmlDictionary();
+
+              xmlDictionary.Add(id);
+              xmlDictionary.Add(ball);
+              xmlDictionary.Add(time);
+              */
+
+
+            string serializedBall = JsonSerializer.Serialize(ballJson);
+
+            queue.Enqueue(serializedBall);
+        }
+
+        public void ToFile()
+        {
+            Thread t = new Thread(() =>
+            {
+                stopWatch.Start();
+                while (true)
+                {
+                    if (/*event na zderzenie*/ stopWatch.ElapsedMilliseconds >= 5)
+                    {
+                        using (StreamWriter sw = File.AppendText(_pathToFile))
+                        {
+                            while (queue.TryDequeue(out string line))
+                            {
+                                sw.WriteLine(line);
+                            }
+                            sw.Flush();
+                            sw.Close();
+                            Thread.Sleep(100);
+                        }
+                    }
+                }
+            })
+            {
+                IsBackground = true
+            };
+            t.Start();
+        }
+
+
+
+        public void stop()
+        {
+            stopWatch.Reset();
+            stopWatch.Stop();
+        }
+    }
+}
