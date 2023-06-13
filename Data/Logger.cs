67985b99cb8a6231855be3748684b7d440c62fe0
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2023-06-13 23:38:13 +0200

    some cleaning

--- Data/Logger.cs
+++ Data/Logger.cs
@@ -1,44 +1,27 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Linq;
 using System.Numerics;
-using System.Text;
 using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Serialization;
 
 namespace Data
 {
     internal class Logger : AbstractLogger
     {
         private readonly string _pathToFile;
-        //private static List<IBall> balls;
         private Stopwatch stopWatch = new Stopwatch();
-        //XmlSerializer serializer = new XmlSerializer(typeof(IBall));
         private static ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
         private SemaphoreSlim _queueSemaphore = new SemaphoreSlim(0);
-        //private readonly Mutex queueMutex = new();
 
-        public Logger(/*List<IBall> balls*/)
+        public Logger()
         {
-            string PathToSave = "../../../../../";
+            string PathToSave = "../../../../";
             _pathToFile = PathToSave + "Logging.json";
 
             if (!File.Exists(_pathToFile))
             {
                 FileStream file = File.Create(_pathToFile);
                 StreamWriter sw = new StreamWriter(file);
-                /*string data = "{\"BallsDiameter\": 20, ": ";
-                sw.WriteLine(data);
-                sw.Flush();
-                sw.Close();*/
             }
-            // balls = balls;
         }
 
         Object _queueLock = new Object();
@@ -72,13 +55,6 @@ namespace Data
                 BallID = id2
             };
 
-            /*  XmlDictionary xmlDictionary = new XmlDictionary();
-
-              xmlDictionary.Add(id);
-              xmlDictionary.Add(ball);
-              xmlDictionary.Add(time);
-              */
-
             string serializedBall1 = JsonSerializer.Serialize(ball1Json);
             string serializedBall2 = JsonSerializer.Serialize(ball2Json);
 
@@ -95,35 +71,6 @@ namespace Data
 
         public override void ToFile()
         {
-            /*Thread t = new Thread(() =>
-            {
-                //stopWatch.Start();
-                while (true)
-                {
-                    if (/*event na zderzenie*//* stopWatch.ElapsedMilliseconds >= 5)
-                    {
-                        lock(_lock)
-                        {
-                            using (StreamWriter sw = File.AppendText(_pathToFile))
-                            {
-                                while (queue.TryDequeue(out string line))
-                                {
-                                    sw.WriteLine(line);
-                                }
-                                sw.Flush();
-                                sw.Close();
-                                Thread.Sleep(100);
-                            }
-                        }
-                        
-                    }
-                }
-            })
-            {
-                IsBackground = true
-            };
-            t.Start();*/
-
             Action<Object> action = async (Object state) =>
             {
                 while (true)
